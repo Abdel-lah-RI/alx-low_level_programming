813245f9d0537d311fa69f4d9bcf7bcaf1fb3036
Abdel-lah-RI <abdellahridoini9@gmail.com>
2023-09-07 12:57:46 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
-#include<stdio.h>
-*/ main is the entry point
-*
-* Return: always 0 = succesful
-*/
-int main(){
-	puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
-	}
+}
