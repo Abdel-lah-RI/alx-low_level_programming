f3ff9a266b89b0abb014daf57197a03936381840
Abdel-lah-RI <abdellahridoini9@gmail.com>
2023-09-20 11:09:18 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int c, j;
 
-	i = 0;
-	while (dest[i])
-		i++;
+	c  = 0;
+	while (dest[c])
+		c++;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[i + j] = src[j];
-	dest[i + j] = '\0';
+		dest[c + j] = src[j];
+	dest[c + j] = '\0';
 	return (dest);
 }
