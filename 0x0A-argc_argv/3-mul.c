999774e9906b8963f4edfaf115822e116d1a7c01
Abdel-lah-RI <abdellahridoini9@gmail.com>
2023-10-02 17:01:17 -0700

    ok

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 /**
  *main-print sum of 2 numbers.
  *@argc:number of commandline arguments.
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		sum = atoi(argv[1] * atoi(argv[2]));
+		sum = (atoi(argv[1]) * atoi(argv[2]));
 		printf("%d\n", sum);
 	}
 	else
