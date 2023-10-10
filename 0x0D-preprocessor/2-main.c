52bc57c9859293ea9e45e6fc98a822bb46f3f86d
Abdel-lah-RI <abdellahridoini9@gmail.com>
2023-10-10 10:22:08 -0700

    the rest of the tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a function that will print the name of the source code file
+ *
+ * return: returns 0 (sucess)
+ */
+int main(void)
+{
+	printf("%s\n",__FILE__);
+
+		return (0);
+}
