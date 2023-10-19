6638d3cb813078f87c861afaedcc46c66f33df7c
Abdel-lah-RI <abdellahridoini9@gmail.com>
2023-10-19 15:35:18 -0700

    ok

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,30 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
-
 /**
-* _strlen - function that returns the length of a string.
-* @s : s is a character
-* Return: value is i
-*/
-int _strlen(const char *s)
-{
-	int i = 0;
-	while (s[i] != '\0')
-	{
-	i++;
-	}
-	return (i);
-}
-
-/**
-* add_node - add a new node at beginning of a list_t list.
-* @head: head of a list_t list.
-* @str: value to insert into element.
-* Return: the number of nodes.
-*/
+ * add_node  - adds a node to the start of the list
+ * @head: address of pointer to head node
+ * @str: str feild of node
+ *
+ * Return: size of list
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *add;
-	add = malloc(sizeof(list_t));
-	if (add == NULL)
-	return (NULL);
-	add->str = strdup(str);
-	add->len = _strlen(str);
-	add->next = *head;
-	*head = add;
+	list_t *new_head = malloc(sizeof(list_t));
 
-	return (add);
+	if (!head || !new_head)
+		return (NULL);
+	if (str)
+	{
+		new_head->str = strdup(str);
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+		new_head->len = _strlen(new_head->str);
+	}
+
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
 }
